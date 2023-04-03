4263f4a8f850d3476d4b038483d9051548cd8576
Furkan <furkantekiroglu@gmail.com>
2023-04-03 12:54:59 +0300

    YoutubeSeri

---
+++ youtubeDersleri/switchCase.java
@@ -0,0 +1,28 @@
+package youtubeDersleri;
+
+public class switchCase {
+    public static void main(String[] args) {
+        char grade = 'C';
+
+        switch (grade) {
+            case 'A':
+                System.out.println("Çok iyi");
+                break;
+            case 'B':
+                System.out.println("iyi");
+                break;
+            case 'C':
+                System.out.println("fena degil");
+                break;
+            case 'D':
+                System.out.println("kotu");
+                break;
+            case 'F':
+                System.out.println("Cok kotu");
+                break;
+            default:
+                System.out.println("Yanlıs karakter girdiniz.");
+                break;
+        }
+    }
+}
