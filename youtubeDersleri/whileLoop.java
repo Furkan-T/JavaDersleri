737c17c22312951fe40fa1adfc2d327c42f0f517
Furkan <furkantekiroglu@gmail.com>
2023-04-03 13:22:14 +0300

    whileLoop

---
+++ youtubeDersleri/whileLoop.java
@@ -0,0 +1,12 @@
+package youtubeDersleri;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while(i<20){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
