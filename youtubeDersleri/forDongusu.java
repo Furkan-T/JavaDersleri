807cba76476cbe4f1f551321e6bcaaf4e70f1223
Furkan <furkantekiroglu@gmail.com>
2023-04-03 12:59:40 +0300

    forLoop

---
+++ youtubeDersleri/forDongusu.java
@@ -0,0 +1,9 @@
+package youtubeDersleri;
+
+public class forDongusu {
+    public static void main(String[] args) {
+        for(int i = 0; i<20; i++){
+            System.out.println("Su anki deger: " +i);
+        }
+    }
+}
