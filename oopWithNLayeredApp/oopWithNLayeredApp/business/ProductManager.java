a48f6077ec6444eda444644d5fff2381ea7f6762
Furkan <furkantekiroglu@gmail.com>
2023-04-11 11:02:48 +0300

    oopWithNLayeredApp

---
+++ oopWithNLayeredApp/oopWithNLayeredApp/business/ProductManager.java
@@ -0,0 +1,32 @@
+package oopWithNLayeredApp.business;
+
+import java.lang.System.Logger;
+import java.util.List;
+
+import oopWithNLayeredApp.dataAccess.HibernateProductDao;
+import oopWithNLayeredApp.dataAccess.JdbcProductDao;
+import oopWithNLayeredApp.dataAccess.ProductDao;
+import oopWithNLayeredApp.entities.Product;
+
+public class ProductManager {
+    private ProductDao productDao;
+    private oopWithNLayeredApp.core.logging.Logger[] loggers;
+
+    
+    public ProductManager(ProductDao productDao, oopWithNLayeredApp.core.logging.Logger[] loggers) {
+        this.productDao = productDao;
+        this.loggers = loggers;
+    }
+
+    public void add(Product product) throws Exception{
+        //iş kuralları
+        if(product.getUnitPrice() < 10){
+            throw new Exception("Ürünün fiyatı 10'dan küçük olamaz");
+        }
+
+        productDao.add(product);
+        for (oopWithNLayeredApp.core.logging.Logger logger : loggers) {
+            logger.log(product.getName());
+        }
+    }
+}
