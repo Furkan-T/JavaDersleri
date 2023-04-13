811d58f49f623983ad9b5aeb2a6563e7901ba9af
Furkan <furkantekiroglu@gmail.com>
2023-04-13 15:14:35 +0300

    Java ders 41

---
+++ youtubeDersleri3745/abstractDemo/MySqlDatabaseManager.java
@@ -0,0 +1,7 @@
+package abstractDemo;
+
+public class MySqlDatabaseManager extends DatabaseManager{
+    public void getData() {
+        System.out.println("Veri getirildi: mySql");
+    }
+}
---
+++ youtubeDersleri3745/abstractDemo/SqlServerDatabaseManager.java
@@ -0,0 +1,10 @@
+package abstractDemo;
+
+public class SqlServerDatabaseManager extends DatabaseManager{
+
+    @Override
+    public void getData() {
+        System.out.println("Veri getirildi: Sql Server");
+    }
+    
+}
