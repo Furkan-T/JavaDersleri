7643dc01cb950a9fe7e1734ab1003f18ae049d50
Furkan <furkantekiroglu@gmail.com>
2023-04-13 15:05:32 +0300

    Java dersi 40

---
+++ youtubeDersleri3745/abstractClasses/Main.java
@@ -0,0 +1,17 @@
+package abstractClasses;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        WomanGameCalculator womanGameColculator = new WomanGameCalculator();
+        womanGameColculator.hesapla();
+        womanGameColculator.gameOver();
+
+        GameCalculator gameCalculator = new WomanGameCalculator();
+        GameCalculator gameCalculator1 = new ManGameCalculator();
+        gameCalculator1.hesapla();
+        gameCalculator1.gameOver();
+        */
+
+    }
+}
