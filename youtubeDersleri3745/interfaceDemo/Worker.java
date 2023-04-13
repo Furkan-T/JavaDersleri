3f0853dac57665da85b5c6c272f7d6927cbd43db
Furkan <furkantekiroglu@gmail.com>
2023-04-13 15:59:07 +0300

    Java dersi 44

---
+++ youtubeDersleri3745/interfaceDemo/Worker.java
@@ -0,0 +1,18 @@
+package interfaceDemo;
+
+public class Worker implements IWorkable,IEatable,IPayible{
+
+    @Override
+    public void work() {
+    }
+
+    @Override
+    public void pay() {
+    }
+
+    @Override
+    public void eat() {
+        
+    }
+    
+}
